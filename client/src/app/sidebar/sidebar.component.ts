dcb2c866f8b2bcc0d528559aadd936a6d1c981b6
skogsfrans <156783701+skogsfrans@users.noreply.github.com>
2026-01-12 10:02:07 +0100

    Add elements to template in sidebar

--- client/src/app/sidebar/sidebar.component.ts
+++ client/src/app/sidebar/sidebar.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
-  templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.scss',
+  imports: [CommonModule],
+  template: `
+    <nav>
+      <button class="new-button"> + Nytt</button>  
+      <button
+        *ngFor="let item of navItems"
+        [ngClass]="item.cssClass">
+        {{ item.icon + item.label }}
+      </button>
+    </nav>
+    <hr/>
+    <p>0 byte av 15 GB används</p>
+    <button class ="get-storage">Få mer lagringsutrymme</button>
+  `,
+  styleUrls: ['./sidebar.component.scss'],
 })
 export class SidebarComponent {
-
+  protected navItems = [
+    { label: 'Startsida', cssClass: 'home-page', icon: '🏠' },
+    { label: 'Min enhet', cssClass: 'my-unit', icon: '📱' },
+    { label: 'Datorer', cssClass: 'computers', icon: '💻' },
+    { label: 'Delas med mig', cssClass: 'shared-with-me', icon: '👫' },
+    { label: 'Senaste', cssClass: 'latest', icon: '🕝' },
+    { label: 'Stjärnmärkt', cssClass: 'marked', icon: '⭐️' },
+    { label: 'Skräppost', cssClass: 'trash', icon: '⚠️' },
+    { label: 'Papperskorg', cssClass: 'bin', icon: '🗑️' },
+    { label: 'Lagring', cssClass: 'storage', icon: '☁️' },
+  ]
 }

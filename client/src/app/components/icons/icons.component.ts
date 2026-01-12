bffc710c550ad78108988a3b7be5577910bb6e89
Viktor Johansson <discovicke@gmail.com>
2026-01-12 13:59:10 +0100

    Add IconsComponent with basic setup and unit test

---
+++ client/src/app/components/icons/icons.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SafeHtml } from '@angular/platform-browser';
+import { IconService } from './icons.service';
+
+@Component({
+  selector: 'app-icon',
+  imports: [CommonModule],
+  template: `<span class="icon" [innerHTML]="svg"></span>`,
+  styles: [`ìcons.component.scss`],
+})
+export class IconsComponent {
+  svg: SafeHtml | null = null;
+
+  @Input() set name(n: string | undefined) {
+    this.svg = n
+      ? this.iconService.get(n)
+      : null;
+  }
+
+  constructor(private iconService: IconService) {}
+}

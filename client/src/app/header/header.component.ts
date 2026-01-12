aea0fbc1fb8617bdbd27c56b4d431a6cf16b99be
skogsfrans <156783701+skogsfrans@users.noreply.github.com>
2026-01-12 10:42:51 +0100

    Add html to template for header comp

--- client/src/app/header/header.component.ts
+++ client/src/app/header/header.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   imports: [],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.scss',
+  template: `
+  <header>
+    <img class="brand-logo" src="/client/public/duck-drive-icon.png" alt="logo" aria-hidden="true" />
+    <h1>Drive</h1>
+    <input class="search" type="text" placeholder="Sök på Drive     ⍯" />
+  </header>
+    `,
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
 

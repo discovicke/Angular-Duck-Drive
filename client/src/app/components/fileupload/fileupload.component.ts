cc7474dac5924ab8cabe753e0a825bf636504faf
skogsfrans <156783701+skogsfrans@users.noreply.github.com>
2026-01-13 13:28:21 +0100

    Add template to fileUpload comp

--- client/src/app/components/fileupload/fileupload.component.ts
+++ client/src/app/components/fileupload/fileupload.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-fileupload',
   imports: [],
-  templateUrl: './fileupload.component.html',
-  styleUrl: './fileupload.component.scss',
+  template: `
+  <input type="file" id="myFile" name="myFile">
+  `,
+
+  styleUrls: ['./fileupload.component.scss'],
 })
 export class FileuploadComponent {
 

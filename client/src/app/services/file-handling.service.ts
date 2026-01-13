37a6fdcb30e7580767b1af815b7623e2b01778ca
skogsfrans <156783701+skogsfrans@users.noreply.github.com>
2026-01-13 14:18:15 +0100

    Add signal file storage service and func to convert file format to Base64

--- client/src/app/services/file-handling.service.ts
+++ client/src/app/services/file-handling.service.ts
@@ -1,8 +1,35 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
+import { FileDto } from '../../../../shared/file-metadata.dto.js';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FileHandlingService {
+
+  content = signal<File | null>(null);
+
+  convertToBase64(content: File): Promise<string> {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onload = () => {
+        const base64 = (reader.result as string).split(',')[1];
+        console.log(resolve(base64));
+      };
+      reader.onerror = reject;
+      reader.readAsDataURL(content);     
+    });
+  }
+
+
+
+  /*
   
+  Metod som konverterar från fil-format till bytes
+
+  Metod som konverterar från bytes till fil-format
+
+  Metod upload för att ladda upp fil - anropa PUT i APIn
+
+  */
+
 }

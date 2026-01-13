549159280528afb8695c3f7d6b443798b60a67c5
Viktor Johansson <discovicke@gmail.com>
2026-01-13 13:40:49 +0100

    Replace header search input with `app-searchbar` component

--- client/src/app/header/header.component.ts
+++ client/src/app/header/header.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
+import {SearchbarComponent} from '../components/searchbar/searchbar.component';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [SearchbarComponent],
   template: `
   <header>
     <img class="brand-logo" src="assets/duck-drive-icon.png" alt="logo" aria-hidden="true" />
     <h1>Drive</h1>
-    <input class="search" type="text" placeholder="Sök på Drive     ⍯" />
+    <app-searchbar></app-searchbar>
   </header>
     `,
   styleUrls: ['./header.component.scss'],

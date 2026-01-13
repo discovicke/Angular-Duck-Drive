e65a2b1055dfbb3d83fcd5fd785f2f594f929f09
Viktor Johansson <discovicke@gmail.com>
2026-01-13 15:23:17 +0100

    Add file upload functionality integrating API PUT request and file conversion to DTO

--- client/src/app/components/fileupload/fileupload.component.ts
+++ client/src/app/components/fileupload/fileupload.component.ts
@@ -5,17 +5,18 @@ import {FileHandlingService} from '../../services/file-handling.service';
   selector: 'app-fileupload',
   imports: [],
   template: `
-  <input type="file" id="myFile" (change)="onFileSelected($event)" name="myFile">
+    <input type="file" id="myFile" (change)="onFileSelected($event)" name="myFile">
   `,
 
   styleUrls: ['./fileupload.component.scss'],
 })
 export class FileuploadComponent {
+  private fileHandlingServiceInjection = inject(FileHandlingService);
 
   onFileSelected(event: any) {
     const file = event.target.files[0];
-    const fileHandlingServiceInjection = inject(FileHandlingService);
-    fileHandlingServiceInjection.content.set(file);
+    this.fileHandlingServiceInjection.content.set(file);
+    this.fileHandlingServiceInjection.uploadFile(file).then(r => console.log(r));
     console.log(file);
   }
 }

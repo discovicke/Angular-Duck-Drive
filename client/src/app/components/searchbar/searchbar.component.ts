88488e58053017347a00632ef85188ec0894910b
Viktor Johansson <discovicke@gmail.com>
2026-01-13 13:40:55 +0100

    Add search icon and update search bar component layout

--- client/src/app/components/searchbar/searchbar.component.ts
+++ client/src/app/components/searchbar/searchbar.component.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
+import {IconService} from '../icons/icons.service';
+import {IconsComponent} from '../icons/icons.component';
 
 @Component({
   selector: 'app-searchbar',
-  imports: [],
-  templateUrl: './searchbar.component.html',
+  imports: [IconsComponent],
+  template: `
+    <div class="searchbar">
+  <span class="searchbar__icon" aria-hidden="true">
+    <!-- Replace with your icons component for magnifier -->
+    <app-icon [name]="'search'"></app-icon>
+  </span>
+
+      <input
+        class="searchbar__input"
+        type="search"
+        placeholder="Sök på Drive"
+        aria-label="Search"
+      />
+    </div>
+  `,
   styleUrl: './searchbar.component.scss',
 })
 export class SearchbarComponent {

e7ceb2ff290a3590a59659538cbf1e2dfbc8f83e
skogsfrans <156783701+skogsfrans@users.noreply.github.com>
2026-01-13 10:08:09 +0100

    Add template and create buttons for aside

--- client/src/app/aside/aside.component.ts
+++ client/src/app/aside/aside.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { NavButtonComponent } from '../components/nav-button/nav-button.component';
 
 @Component({
   selector: 'app-aside',
-  imports: [],
-  templateUrl: './aside.component.html',
+  imports: [NavButtonComponent],
+  template: `
+  <aside>
+    <nav>
+      <app-nav-button label="Nytt" icon="plus" variant="primary" cssClass="aside-button" />
+      @for (item of asideItems; track item.label) {
+      <app-nav-button [label]="item.label" [icon]="item.icon" [cssClass]="item.cssClass" />
+      }
+    </nav>
+  <aside>
+  `,
   styleUrl: './aside.component.scss',
 })
 export class AsideComponent {
-
+  protected readonly asideItems = [
+    { label: 'Keep', cssClass: 'keep', icon: 'keep' },
+    { label: 'Tasks', cssClass: 'tasks', icon: 'task' },
+    { label: 'Kontakter', cssClass: 'contacts', icon: 'contacts' },
+    { label: 'Hämta tillägg', cssClass: 'get-add-ons', icon: 'add-on' },
+  ];
 }

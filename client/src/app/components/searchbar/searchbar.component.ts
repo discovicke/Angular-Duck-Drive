8629bd1a7534bbfc2a5adc1d3275cfe8940cc60f
skogsfrans <156783701+skogsfrans@users.noreply.github.com>
2026-01-14 11:05:18 +0100

    Add function to call on search function from searchbar comp

--- client/src/app/components/searchbar/searchbar.component.ts
+++ client/src/app/components/searchbar/searchbar.component.ts
@@ -1,25 +1,31 @@
-import { Component } from '@angular/core';
-import {IconService} from '../icons/icons.service';
-import {IconsComponent} from '../icons/icons.component';
+import { Component, inject } from '@angular/core';
+import { IconsComponent } from '../icons/icons.component';
+import { FileHandlingService } from '../../services/file-handling.service';
 
 @Component({
   selector: 'app-searchbar',
   imports: [IconsComponent],
   template: `
     <div class="searchbar">
-  <span class="searchbar__icon" aria-hidden="true">
-    <app-icon [name]="'search'"></app-icon>
-  </span>
+      <span class="searchbar__icon" aria-hidden="true">
+        <app-icon [name]="'search'"></app-icon>
+      </span>
       <input
         class="searchbar__input"
         type="search"
         placeholder="Sök på Drive"
         aria-label="Search"
+        (input)="onSearch($event.target.value)"
       />
     </div>
   `,
   styleUrl: './searchbar.component.scss',
 })
 export class SearchbarComponent {
+  FileService = inject(FileHandlingService);
+
+  onSearch(value: string) {
+    this.FileService.searchAllFiles(value);
+  }
 
 }

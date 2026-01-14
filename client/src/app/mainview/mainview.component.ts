2682cf216682bd9f0af98e2eda79e6d47344ad08
discovicke <discovicke@gmail.com>
2026-01-14 11:39:41 +0100

    Add empty state display for file list when no files are available

--- client/src/app/mainview/mainview.component.ts
+++ client/src/app/mainview/mainview.component.ts
@@ -7,29 +7,46 @@ import { FileHandlingService } from '../services/file-handling.service';
   imports: [FilerowComponent],
   template: `
     <div class="mainview">
-      <div class="file-list-header">
-        <span class="header-name">Namn</span>
-        <span class="header-owner">Ägare</span>
-        <span class="header-date">Uppladdad</span>
-        <span class="header-date">Ändrad</span>
-        <span class="header-size">Storlek</span>
-        <span class="header-action"></span>
-      </div>
-      <div class="file-list">
-        @for (filerow of FileService.filesList(); track filerow.fileName) {
-          <app-filerow
-            [fileName]="filerow.fileName"
-            [ownerName]="filerow.ownerName"
-            [uploadedAt]="filerow.uploadedAt"
-            [editedAt]="filerow.editedAt"
-            [sizeInBytes]="filerow.sizeInBytes"
-          ></app-filerow>
-        }
-      </div>
+      @if (FileService.filesList().length > 0) {
+        <div class="file-list-header">
+          <span class="header-name">Namn</span>
+          <span class="header-owner">Ägare</span>
+          <span class="header-date">Uppladdad</span>
+          <span class="header-date">Ändrad</span>
+          <span class="header-size">Storlek</span>
+          <span class="header-action"></span>
+        </div>
+        <div class="file-list">
+          @for (filerow of FileService.filesList(); track filerow.fileName) {
+            <app-filerow
+              [fileName]="filerow.fileName"
+              [ownerName]="filerow.ownerName"
+              [uploadedAt]="filerow.uploadedAt"
+              [editedAt]="filerow.editedAt"
+              [sizeInBytes]="filerow.sizeInBytes"
+            ></app-filerow>
+          }
+        </div>
+      } @else {
+        <div class="empty-state">
+          <img [src]="randomEmptyStateImage" alt="Inga filer" />
+          <p>Ooops... Inga filer att visa!</p>
+        </div>
+      }
     </div>
   `,
   styleUrl: './mainview.component.scss',
 })
 export class MainviewComponent {
   FileService = inject(FileHandlingService);
+
+  private emptyStateImages = [
+    'assets/empty-state-1.png',
+    'assets/empty-state-2.png',
+    'assets/empty-state-3.png',
+  ];
+
+  randomEmptyStateImage = this.emptyStateImages[
+    Math.floor(Math.random() * this.emptyStateImages.length)
+  ];
 }

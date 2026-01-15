868f64aa2903724b16dd7feebe3ff1f2bd2b685e
skogsfrans <156783701+skogsfrans@users.noreply.github.com>
2026-01-15 15:51:01 +0100

    Add functionality for drag-and-drop in mainview

--- client/src/app/directives/drag-and-drop.directive.ts
+++ client/src/app/directives/drag-and-drop.directive.ts
@@ -1,10 +1,32 @@
-import { Directive } from '@angular/core';
+import { Directive, HostListener, signal, output } from '@angular/core';
 
 @Directive({
   selector: '[appDragAndDrop]',
 })
 export class DragAndDropDirective {
 
-  constructor() { }
+  fileDropped = output<File>();
 
+  @HostListener('dragover', ['$event'])
+  handleOnDragover(event: DragEvent): void {
+    event.preventDefault();
+    console.log('nu drar jag över');
+  }
+
+  @HostListener('dragleave', ['$event'])
+  handleLeave(event: DragEvent): void {
+    event.preventDefault();
+    console.log('nu drar jag iväg');
+  }
+
+  @HostListener('drop', ['$event'])
+  handleDrop(event: DragEvent): void {
+    event.preventDefault();
+    event.stopPropagation();
+    const file = event.dataTransfer?.files[0];
+    console.log('nu släpper jag');
+    if (file) {
+      this.fileDropped.emit(file);
+    }
+  }
 }
--- client/src/app/mainview/mainview.component.ts
+++ client/src/app/mainview/mainview.component.ts
@@ -1,44 +1,51 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
 import { FilerowComponent } from '../components/filerow/filerow.component';
 import { FileHandlingService } from '../services/file-handling.service';
+import { DragAndDropDirective } from '../directives/drag-and-drop.directive';
 
 @Component({
   selector: 'app-mainview',
-  imports: [FilerowComponent],
+  imports: [FilerowComponent, DragAndDropDirective],
   template: `
-    <div class="mainview">
-      @if (FileService.filesList().length > 0) {
-        <div class="file-list-header">
-          <span class="header-name">Namn</span>
-          <span class="header-owner">Ägare</span>
-          <span class="header-date" id="headerUploadedAt">Uppladdad</span>
-          <span class="header-date">Ändrad</span>
-          <span class="header-size">Storlek</span>
-          <span class="header-action"></span>
-        </div>
-        <div class="file-list">
-          @for (filerow of FileService.filesList(); track filerow.fileName) {
-            <app-filerow
-              [fileName]="filerow.fileName"
-              [ownerName]="filerow.ownerName"
-              [uploadedAt]="filerow.uploadedAt"
-              [editedAt]="filerow.editedAt"
-              [sizeInBytes]="filerow.sizeInBytes"
-            ></app-filerow>
-          }
-        </div>
-      } @else {
-        <div class="empty-state">
-          <img [src]="randomEmptyStateImage" alt="Inga filer" />
-          <p>Ooops... Inga filer att visa!</p>
-        </div>
-      }
-    </div>
+    <div class="mainview" appDragAndDrop (fileDropped)="onFileDropped($event)" >
+        @if (FileService.filesList().length > 0) {
+          <div class="file-list-header">
+            <span class="header-name">Namn</span>
+            <span class="header-owner">Ägare</span>
+            <span class="header-date" id="headerUploadedAt">Uppladdad</span>
+            <span class="header-date">Ändrad</span>
+            <span class="header-size">Storlek</span>
+            <span class="header-action"></span>
+          </div>
+          <div class="file-list">
+            @for (filerow of FileService.filesList(); track filerow.fileName) {
+              <app-filerow
+                [fileName]="filerow.fileName"
+                [ownerName]="filerow.ownerName"
+                [uploadedAt]="filerow.uploadedAt"
+                [editedAt]="filerow.editedAt"
+                [sizeInBytes]="filerow.sizeInBytes"
+              ></app-filerow>
+            }
+          </div>
+        } @else {
+          <div class="empty-state">
+            <img [src]="randomEmptyStateImage" alt="Inga filer" />
+            <p>Ooops... Inga filer att visa!</p>
+          </div>
+        }
+      </div>
   `,
   styleUrl: './mainview.component.scss',
 })
 export class MainviewComponent {
   FileService = inject(FileHandlingService);
+
+  fileReceived = input<File>();
+
+  onFileDropped(fileReceived : File) : void {
+    this.FileService.uploadFile(fileReceived);
+  }
 
   private emptyStateImages = [
     'assets/empty-state-1.png',
